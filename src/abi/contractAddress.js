fd1b13f8347d2e9e77986a077d42aa30309f1664
srdothunter <srdothunter@gmail.com>
2021-03-11 16:22:21 +0800

    commit

--- src/abi/contractAddress.js
+++
@@ -1,4 +0,0 @@
-const mainAddress = {
-  main: "5Es1daxrUQbVi5w41wghTVnuwQAABc5Sw4E1mHVtuZ7guhdQ"
-}
-export default mainAddress;
--- src/utils/publicJs.js
+++
@@ -1,9 +0,0 @@
-const formatResult =  (result) =>{
-    let str;
-    if (result && result.output) {
-        str = result.output.toHuman();
-    }
-    return str;
-}
-
-export default{ formatResult }
